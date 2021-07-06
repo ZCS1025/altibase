f3b4b7ba6c7b47623ccaf51a193c3017c8c7dcbb
jasonpark3306 <jason.park@altibase.com>
2021-07-06 19:53:32 +0900

    trunk-20210706

--- src/src/cm/cmEnv.mk
+++
@@ -1,5 +0,0 @@
-include $(dir $(word $(words $(MAKEFILE_LIST)),$(MAKEFILE_LIST)))/../../env.mk
-include $(CM_DIR)/lib/cm_objs.mk
-
-INCLUDES += $(foreach i,$(CORE_EXT_DIR)/include $(CM_DIR)/include,$(IDROPT)$(i))
-
